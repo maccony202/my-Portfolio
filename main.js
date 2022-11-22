d70d194933ad8a7987238084ce35c118ee2baf78
maccony202 <73612968+maccony202@users.noreply.github.com>
2022-11-22 11:28:41 +0000

    Add changes to main.js & css file

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
+// humburger popup menu page transition
+const humberger = document.querySelector('.humberger');
+const navMenu = document.querySelector('.nav-links');
 
+humberger.addEventListener('click', () => {
+  humberger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-//humburger popup menu page transition
-const humberger = document.querySelector(".humberger");
-const navMenu = document.querySelector(".nav-links");
-const mobileLogo = document.querySelector(".moblogo");
-const portfo = document.querySelector("#port");
-const aboutM = document.querySelector("#about");
-const conts = document.querySelector("#cont");
-
-humberger.addEventListener("click", () => {
-    humberger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-links").forEach(n => n.
-    addEventListener("click", () => {
-        humberger.classList.remove("active");
-        navMenu.classList.remove("active");
-       
-    }))
-
-
-
- 
-
-    
-
+document.querySelectorAll('.nav-links').forEach((n) => n
+  .addEventListener('click', () => {
+    humberger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
